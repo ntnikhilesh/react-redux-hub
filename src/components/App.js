c8eb7bdaacf3bce8820bc8b6984325904351f833
ntnikhilesh <ntnikhilesh@gmail.com>
2018-01-19 16:26:35 +0530

    redux installation done

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        React with Redux
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+//react with redux
+
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
